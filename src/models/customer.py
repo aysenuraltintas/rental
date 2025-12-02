ef040123c53b2ff4feaef014ef29b618c88fcfdd
aysenuraltintas <ayshenuraltintas@gmail.com>
2025-12-02 04:21:43 +0300

    Müşteri ekleme fonksiyonu geliştirildi

---
+++ src/models/customer.py
@@ -0,0 +1,7 @@
+def __init__(self, id, name, phone):
+        self.id = id
+        self.name = name
+        self.phone = phone
+
+def to_line(self):
+    return f"{self.id};{self.name};{self.phone}\n"
